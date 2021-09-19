0f5d5d718e9411da9aa9298b75d4015371d159e3
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-19 10:30:01 -0700

    add reset.css

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ $(document).ready(function(){
         }
     });
 });
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["IOS Developer", "Web-Developer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["IOS Developer", "Web-Developer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
