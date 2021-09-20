0a10a75490bbd94f4185283fa336ca17de50387a
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-20 08:52:02 -0700

    initial commit of day to see situation

--- script.js
+++ script.js
@@ -22,31 +22,3 @@ var typed = new Typed(".typing-2", {
     backSpeed: 60,
     loop: true
 });
-
-// hamburger menu focus
-var menuToggle = document.querySelector("#menu-toggle");
-var menu = document.querySelector("#menu");
-
-menuToggle.addEventListener("click", function(event) {
-    var menuOpen = menu.checked;
-
-    if(menuOpen) {
-        menu.checked = false;
-    } else {
-        menu.checked = true;
-    }
-});
-if(navOpen) {
-    nav.checked = false;
-    navToggle.setAttribute("aria-expanded", false);
-} else {
-    nav.checked = true;
-    navToggle.setAttribute("aria-expanded", true);
-}
-navToggle.addEventListener("click", function(event) {
-    var navOpen = nav.classList.contains("active");
-    var newNavOpenStatus = !navOpen;
-
-    navToggle.setAttribute("aria-expanded", newNavOpenStatus);
-    nav.classList.toggle("active");
-});
