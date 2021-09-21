851d5c9ccf64bd67bcc3dd9cfe703200ffaab5b2
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-21 10:48:09 -0700

    add web gif

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         // sticky navbar on scroll script
-//         if(this.scrollY > 20){
-//             $('.navbar').addClass("sticky");
-//         }else{
-//             $('.navbar').removeClass("sticky");
-//         }
-//     });
-// });
 
  // typing text animation script
  var typed = new Typed(".typing", {
@@ -22,6 +12,3 @@ var typed = new Typed(".typing-2", {
     backSpeed: 60,
     loop: true
 });
-
-
-//  show gifs when mouse over
